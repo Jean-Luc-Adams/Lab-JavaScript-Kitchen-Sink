200f0f74dc7895c5a150c6e3c95ac22f6d3fe570
Jean-Luc-Adams <jladams91@gmail.com>
2018-04-17 22:25:21 -0500

    First 7 objectives done

---
+++ app.js
@@ -0,0 +1,15 @@
+var name = 'Jean-Luc';
+
+const states = 50;
+
+
+var sum = 5 + 4;
+
+/* If your name starts with a letter after L(ASCII code: 76), give first message, if it's before give second message */
+if (name.charCodeAt(0) > 76) {
+    alert('Back of the line!');
+} else {
+    alert('Next!');
+}
+
+

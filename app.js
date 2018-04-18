b37b6245ebd37fa3294b13e3a1263cbf94b8d2b2
Jean-Luc-Adams <jladams91@gmail.com>
2018-04-18 01:58:27 -0500

    Lab completed!

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ sayHello();
 /* A function for checks your age to see if you are 21 are not */
 function checkAge(name, age) {
     if (age < 21) {
-        alert("Sorry " + name + " you aren't old enough to view this page!")
+        alert("Sorry " + name + ", you aren't old enough to view this page!")
     }
 }
 
@@ -36,10 +36,38 @@ checkAge('James', 18);
 checkAge('John', 17);
 
 /* A list of favorite vegetables */
-let favVeggie = ['corn', 'green beans', 'potatoes', 'califlower', 'cucumber'];
+let favVeggie = ['corn', 'spinach', 'potatoes', 'califlower', 'cucumber'];
 
 /* Use a loop to display the veggies in the console */
-for (let i = 0; i < favVeggie.length; i++) {
-    console.log(favVeggie[i]); 
+for (i = 0; i < favVeggie.length; i++) {
+    console.log(favVeggie[i]);
 }
 
+/* Create an array of 5 objects with name and the age */
+let people = [
+    {name: 'Jeff', age: 16},
+    {name: 'Mary', age: 25},
+    {name: 'Daniel', age: 21},
+    {name: 'Tommy', age: 20},
+    {name: 'Rebecca', age: 23},
+]
+
+/* Call checkAge for the array above */
+for (i = 0; i < people.length; i++) {
+    checkAge(people[i].name, people[i].age);
+}
+
+/* Create function that checks how many characters are in a string */
+function getLength(word) {
+    return word.length;
+}
+
+/* Call function with Hello World */
+let result = getLength('Hello Word');
+
+/* Created an if else statement that show said string has either a even or odd number of characters in string */
+if (result % 2 == 0) {
+    console.log('The world is nice and even!');
+} else {
+    console.log('The world is an odd place!');
+}
